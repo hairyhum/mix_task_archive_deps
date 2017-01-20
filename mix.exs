a77e8cbc05f52d3c9f29cc72aa2577aaf0ceca0c
Daniil Fedotov <dfedotov@pivotal.io>
2017-01-20 17:48:20 +0000

    License and packaging

--- mix.exs
+++ mix.exs
@@ -1,3 +1,22 @@
+##
+##
+## Copyright (c) 2017, Daniil Fedotov.
+## All Rights Reserved.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+##
+
 defmodule MixTaskDepsArchive.Mixfile do
   use Mix.Project
 
@@ -7,6 +26,8 @@ defmodule MixTaskDepsArchive.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: "Mix task to create archives for a project dependencies and elixit itslef",
+     package: package(),
      deps: deps()]
   end
 
@@ -29,5 +50,12 @@ defmodule MixTaskDepsArchive.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+  defp package() do
+    [name: :mix_task_deps_archive,
+     files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Daniil Fedotov"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/hairyhum/mix_task_deps_archive"}]
   end
 end
